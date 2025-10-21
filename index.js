7ef88caab155599609c89af61a49d179586d6cea
Luan <lsantana.duarte7@gmail.com>
2025-10-21 17:21:39 -0300

    aprendendo classes e métodos abstratos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import { Cliente } from './Cliente.js';
 import { ContaCorrente } from './ContaCorrente.js';
 import { ContaPoupança } from './ContaPoupança.js';
+import { ContaSalario } from './ContaSalario.js';
 
 // cria um novo cliente
 const cliente1 = new Cliente('Ricardo', 11122233309)
@@ -12,7 +13,9 @@ contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
 const ContaPoupanca = new ContaPoupança(50, cliente1, 1001);
-ContaPoupanca.sacar(10);
 
-console.log(contaCorrenteRicardo);
-console.log(ContaPoupanca);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+
+console.log(contaSalario);
