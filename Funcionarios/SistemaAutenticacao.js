8b9e29ca67c0488563fa8641cdf4992975ab8a70
luansantanaduarte <lsantana.duarte7@gmail.com>
2025-10-23 16:42:52 -0300

    finalizando o projeto

--- Funcionarios/SistemaAutenticacao.js
+++ Funcionarios/SistemaAutenticacao.js
@@ -1,6 +1,14 @@
 export class SistemaAutenticacao {
     static login(autenticavel, senha) {
         // Polimorfismo -> Capacidade de um objeto de se adequar a diferentres outros métodos de maneira genérica
-        return autenticavel.autenticar(senha);
+        if (SistemaAutenticacao.eAutenticavel(autenticavel)) {
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static eAutenticavel(autenticavel) {
+        // identifica se o método existe na classe e se, de fato, o método é uma instancia de função.
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function
     }
 }
